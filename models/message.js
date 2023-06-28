5a47eb6377e840774d323ee44ad0976feb9a1a8f
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-28 17:21:01 -0300

    add style

--- models/message.js
+++ models/message.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    user: { type: String, required: true },
-    text: { type: String, required: true },
+    user: {
+      type: String,
+      required: [true, "Name is required."],
+      minlength: [3, "Name must be at least 3 characters long."],
+      maxlength: [30, "Name must be below 30 characters long."],
+    },
+    text: {
+      type: String,
+      required: [true, "Message is required."],
+      minlength: [3, "Message must be at least 3 characters long."],
+      maxlength: [140, "Message must be below 140 characters long."],
+    },
   },
   { timestamps: true }
 );
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
