411523e95d373f8101d381cb4a6b1a58450155ab
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-30 16:48:18 -0300

    handle errors

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,25 @@ const Message = require("../models/message");
 
 /* GET Message Board*/
 router.get("/", function (req, res, next) {
-  Message.find().sort({ createdAt: -1 }).then((data) =>
-    res.render("index", { title: "Message Board", messages: data })
-  );
+  Message.find()
+    .sort({ createdAt: -1 })
+    .then((data) =>
+      res.render("index", { title: "Message Board", messages: data })
+    );
 });
 
 /* GET New message form. */
 router.get("/new", function (req, res, next) {
-  res.render("form");
+  res.render("form", { errors: null });
 });
 
 /* Post Message. */
 router.post("/new", function (req, res, next) {
-  Message.create({ ...req.body }).then(() => res.redirect("/"));
+  Message.create({ ...req.body })
+    .then(() => res.redirect("/"))
+    .catch((error) => {
+      res.render("form", { errors: error.errors });
+    });
 });
 
 module.exports = router;
